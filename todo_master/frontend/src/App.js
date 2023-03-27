1d1cf81af41a1fead62d99d1d92541fcc15a23fa
Mariya <mariyaanatolevna86@yandex.ru>
2023-03-28 00:36:07 +0300

    changed lesson_7

--- todo_master/frontend/src/App.js
+++ todo_master/frontend/src/App.js
@@ -11,7 +11,7 @@ import NotFound from './components/NotFound.js';
 import LoginForm from './components/Auth.js';
 import{Route, BrowserRouter, Link, Switch} from 'react-router-dom';
 
-import Cookies from 'universal=cookie';
+import Cookies from 'universal-cookie';
 
 
 const DOMAIN = 'http://127.0.0.1:8000/api'
@@ -28,13 +28,47 @@ class App extends React.Component {
             'token': ''
         }
     }
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, ()=>this.load_data())
+    }
+    is_authenticated() {
+        return this.state.token != ''
+    }
+    logout() {
+        this.set_token('')
+    }
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token}, ()=>this.load_data())
+    }
+    get_token(username, password) {
+        axios.post('http://127.0.0.1:8000/api-token-auth/', {username: username,
+    password: password})
+        .then(response => {
+            this.set_token(response.data['token'])
+        }).catch(error => alert('Неверный логин или пароль'))
+    }
+    get_headers() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_authenticated()){
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+    }
 
-    load_data(){
+    load_data() {
         const headers = this.get_headers()
-
         axios.get(get_url('/userapp'), {headers}).then(response => {
             this.setState({'userapp': response.data.results})
-            }).catch(error => alert('Не верный логин или пароль.'))
+            }).catch(error => {
+                alert('Не верный логин или пароль.')
+                this.setState({userapp: []})
+            })
 
         axios.get(get_url('/project'), {headers}).then(response => {
             this.setState({'projects': response.data.results})
@@ -44,47 +78,10 @@ class App extends React.Component {
             this.setState({'todo': response.data.results})
             }).catch(error => console.log(error))
     }
-    set_token(token){
-        const cookies = new Cookies()
-        cookies.set('token', token)
-        this.setState({'token': token}, ()=>this.load_data())
-    }
-
-    get_token(username, password) {
-        axios.post(get_url('/api-token-auth'),
-            {'username': username, 'password': password})
-            .then(response => {
-                this.set_token(response.data['token'])
-            }).catch(error => console.log(error))
-    }
-
-    is_auth(){
-        return !!this.state.token
-    }
-
-    get_headers(){
-        let headers = {
-            'Content_Type': 'applications/json'
-        }
-
-        if(this.is_auth()){
-            headers['Authorization'] = `Token ${this.state.token}`
-        }
-
-        return headers
-    }
-
-    logout() {
-        this.set_token('')
-    }
-    get_token_from_cookies(){
-        const cookies = new Cookies()
-        const token = cookies.get('token')
-        this.setState({'token': token}, ()=>this.load_data())
-    }
     componentDidMount() {
-        this.get_token_from_cookies()
+        this.get_token_from_storage()
     }
+
 
     render() {
         return (
@@ -92,7 +89,18 @@ class App extends React.Component {
                 <BrowserRouter>
                     <div>
                         <header>
-                            <Menu/>
+                            <nav class='top-menu'>
+                                <a class='navbar-logo' href='#'><img src='../logo.svg' alt='Этот логотип не загрузился'/></a>
+                                <ul class='menu-main'>
+                                    <li><Link to='/'>Users</Link></li>
+                                    <li><Link to='/projects'>Projects</Link></li>
+                                    <li><Link to='/todo'>Todo</Link></li>
+                                    <li>
+                                        {this.is_authenticated() ? <button class='menu-main' onClick={()=>this.logout()}>Logout</button> :
+                                        <Link to='/login'>Login</Link>}
+                                    </li>
+                                </ul>
+                            </nav>
                         </header>
                     </div>
                     <div>
@@ -100,7 +108,7 @@ class App extends React.Component {
                             <Route exact path='/' component={() => <UserList userapp={this.state.userapp}/>}/>
                             <Route exact path='/projects' component={() => <ProjectList projects={this.state.projects}/>}/>
                             <Route exact path='/todo' component={() => <TodotList todo={this.state.todo}/>}/>
-                            <Route exact path='/login' component={() => <LoginForm get_token={(username, password)=>this.get_token=(username, password)}/>}/>
+                            <Route exact path='/login' component={() => <LoginForm get_token={(username, password) => this.get_token(username, password)}/>}/>
                             <Route component={NotFound}/>
                         </Switch>
                     </div>
--- todo_master/frontend/src/components/Menu.js
+++ todo_master/frontend/src/components/Menu.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import logo from '../logo.svg';
-import {Link} from 'react-router-dom'
-
-
-
-export default function Menu() {
-
-    return(
-        <nav class='top-menu'>
-            <a class='navbar-logo' href='#'><img src='../logo.svg' alt='Этот логотип не загрузился'/></a>
-            <ul class='menu-main'>
-                <li><Link to='/'>Users</Link></li>
-                <li><Link to='/projects'>Projects</Link></li>
-                <li><Link to='/todo'>Todo</Link></li>
-            </ul>
-        </nav>
-    )
-}
+//import React from 'react';
+//import logo from '../logo.svg';
+//import {Link} from 'react-router-dom'
+//import '../App.js'
+//
+//
+//
+//export default function Menu() {
+//
+//    return(
+//
+//    )
+//}
 
