09ce1e639e61072fed6d689e1a6343bd040881ff
Mariya <mariyaanatolevna86@yandex.ru>
2023-03-27 19:48:01 +0300

    lesson_7

--- todo_master/frontend/src/App.js
+++ todo_master/frontend/src/App.js
@@ -8,7 +8,10 @@ import TodotList from './components/Todo.js';
 import Menu from './components/Menu.js';
 import Footer from './components/Footer.js';
 import NotFound from './components/NotFound.js';
+import LoginForm from './components/Auth.js';
 import{Route, BrowserRouter, Link, Switch} from 'react-router-dom';
+
+import Cookies from 'universal=cookie';
 
 
 const DOMAIN = 'http://127.0.0.1:8000/api'
@@ -21,39 +24,67 @@ class App extends React.Component {
         this.state = {
             'userapp': [],
             'projects': [],
-            'todo': []
+            'todo': [],
+            'token': ''
         }
     }
 
-    componentDidMount() {
-        /*const userapp = [
-            {
-            'username': 'Василисушка',
-            'first_name': 'Василиса',
-            'last_name': 'Прекрасная',
-            'email': 'Vasilisa@mail.ru'
-            },
-            {
-            'username': 'Серый@',
-            'first_name': 'Волк',
-            'last_name': 'Серый',
-            'email': 'Seriiy@gav.me'
-            }
-        ] */
+    load_data(){
+        const headers = this.get_headers()
 
-        axios.get(get_url('/userapp')).then(response => {
+        axios.get(get_url('/userapp'), {headers}).then(response => {
             this.setState({'userapp': response.data.results})
-            }).catch(error => console.log(error))
+            }).catch(error => alert('Не верный логин или пароль.'))
 
-        axios.get(get_url('/project')).then(response => {
+        axios.get(get_url('/project'), {headers}).then(response => {
             this.setState({'projects': response.data.results})
             }).catch(error => console.log(error))
 
-        axios.get(get_url('/todo')).then(response => {
+        axios.get(get_url('/todo'), {headers}).then(response => {
             this.setState({'todo': response.data.results})
             }).catch(error => console.log(error))
+    }
+    set_token(token){
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, ()=>this.load_data())
+    }
 
+    get_token(username, password) {
+        axios.post(get_url('/api-token-auth'),
+            {'username': username, 'password': password})
+            .then(response => {
+                this.set_token(response.data['token'])
+            }).catch(error => console.log(error))
+    }
+
+    is_auth(){
+        return !!this.state.token
+    }
+
+    get_headers(){
+        let headers = {
+            'Content_Type': 'applications/json'
         }
+
+        if(this.is_auth()){
+            headers['Authorization'] = `Token ${this.state.token}`
+        }
+
+        return headers
+    }
+
+    logout() {
+        this.set_token('')
+    }
+    get_token_from_cookies(){
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token}, ()=>this.load_data())
+    }
+    componentDidMount() {
+        this.get_token_from_cookies()
+    }
 
     render() {
         return (
@@ -69,6 +100,7 @@ class App extends React.Component {
                             <Route exact path='/' component={() => <UserList userapp={this.state.userapp}/>}/>
                             <Route exact path='/projects' component={() => <ProjectList projects={this.state.projects}/>}/>
                             <Route exact path='/todo' component={() => <TodotList todo={this.state.todo}/>}/>
+                            <Route exact path='/login' component={() => <LoginForm get_token={(username, password)=>this.get_token=(username, password)}/>}/>
                             <Route component={NotFound}/>
                         </Switch>
                     </div>
