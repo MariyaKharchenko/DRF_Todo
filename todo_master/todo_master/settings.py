c15114e455e918f51ea3d4f91490d7825bfd4faf
Mariya <mariyaanatolevna86@yandex.ru>
2023-03-30 02:05:06 +0300

    lesson_9

--- todo_master/todo_master/settings.py
+++ todo_master/todo_master/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'todoapp',
     'django_filters',
     'rest_framework.authtoken',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -153,6 +154,8 @@ REST_FRAMEWORK = {
         ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
 }
 
