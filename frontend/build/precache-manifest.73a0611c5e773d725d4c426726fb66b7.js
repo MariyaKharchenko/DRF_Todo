04d2e59885ead81ec4508e8da16a22c26ead5590
Mariya <mariyaanatolevna86@yandex.ru>
2023-04-26 19:11:24 +0300

    lesson_12 changed

---
+++ frontend/build/precache-manifest.73a0611c5e773d725d4c426726fb66b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45c357102180339622db",
+    "url": "/static/css/main.4ed15307.chunk.css"
+  },
+  {
+    "revision": "45c357102180339622db",
+    "url": "/static/js/main.45c35710.chunk.js"
+  },
+  {
+    "revision": "e5846861b89810b99e9d",
+    "url": "/static/js/1.e5846861.chunk.js"
+  },
+  {
+    "revision": "7a8f4fec34b36578ef15",
+    "url": "/static/js/2.7a8f4fec.chunk.js"
+  },
+  {
+    "revision": "9167b91419df04aaa064",
+    "url": "/static/js/runtime~main.9167b914.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "533707b4670318f4cde4065de48d7de6",
+    "url": "/index.html"
+  }
+];
